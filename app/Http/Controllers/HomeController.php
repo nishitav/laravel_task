904615560e13d6b56ce14aeb69c22b8f78233e54
Nishit Vadhwana <vadhwananishit@live.com>
2022-01-08 12:29:57 +0530

    Homocontroller login and logout action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    //
+class HomeController extends Controller {
+    /*
+     * Login operation  
+     */
+    public function login(Request $request) {
+        // validate request
+        $this->validate($request, [
+            'email' => 'bail|required|email',
+            'password' => 'bail|required|min:6',
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            return response()->json([
+                'msg' => 'You are logged in succeessfully.',
+                'user' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'msg' => 'Incorrect login details',
+            ], 401);
+        }
+    }
+
+    /*
+     * Logout operation 
+     */
+
+    public function logout() {
+        Auth::logout();
+        return redirect('/login');
+    }
+
 }
