b4b6ba1da54c775944475aa43db7fddae7001646
Nishit Vadhwana <vadhwananishit@live.com>
2022-01-09 00:02:37 +0530

    Vue main app file

---
+++ resources/js/components/mainapp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <div>
+      <div class="_1side_menu" >
+        <div class="_1side_menu_logo">
+          <h3 style="text-align:center;">DEMO APP</h3>
+        </div>
+        <div class="_1side_menu_content">
+          <div class="_1side_menu_list">
+            <ul class="_1side_menu_list_ul">
+                <li><router-link to="products">Products</router-link></li>
+                <li><a href="/logout">Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="header">
+        <div class="_2menu _box_shadow">
+          <div class="_2menu_logo">
+            <ul class="open_button">
+              <li>
+                <Icon type="ios-list" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    	<router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['user'],
+    data(){
+       return {
+          isLoggedIn : false,
+       }
+    }
+}
+</script>
