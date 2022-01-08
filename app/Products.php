c17f199096a517b69d902105c6336d4c9fdb6c37
Nishit Vadhwana <vadhwananishit@live.com>
2022-01-08 11:13:25 +0530

    Product Model file

---
+++ app/Products.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $fillable = ['name','price','upc','status','image'];
+    
+    /**
+     * Model validation rules
+    */
+    public static function validationRules() {
+        $results = [
+            'name' => 'required',
+            'price' => 'required',
+            'upc' => 'required',
+            'status' => 'required',
+            'image' => 'required',
+        ];
+
+        return $results;
+    }
+    
+}
